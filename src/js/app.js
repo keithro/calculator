3a7514426d246e2380512da78ff7cd9ae7686e23
Keith Rodriguez <rodriguez.keith@gmail.com>
2018-03-13 20:55:07 -0400

    Initial commit

---
+++ src/js/app.js
@@ -0,0 +1,17 @@
+import 'normalize.css/normalize.css';
+import '../sass/style.scss';
+
+console.log('Hello from app.js and webpack dev server!');
+
+// previous numbers array (for display)
+// current number
+// total variable
+
+// get data attribute
+
+// set to current val variable
+
+// if + - * / combine string and convert to number
+
+
+
